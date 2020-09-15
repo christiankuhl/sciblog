f04a5a32f0e29d3273dbc94adc7b96082d8e3f0b
Christian Kuhl <christian.kuhl84@gmail.com>
2020-09-15 23:48:11 +0200

    Idiots be idiots

--- sciblog/settings.py
+++ sciblog/settings.py
@@ -8,7 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -141,4 +141,4 @@ CKEDITOR_IMAGE_BACKEND = "pillow"
 #    },
 #}
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
